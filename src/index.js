67f61291d1639984851c18a6d22fd1555194a2b1
Anirudh Parui <alwaysanirudh@gmail.com>
2017-09-10 17:40:35 +0530

    Setup the html and css

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import {Provider} from 'react-redux';
+import configureStore from './store/configureStore';
+import ContactsComponent from './components/ContactsComponent';
+import {loadContacts} from './actions/contactActions';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const store = configureStore();
+store.dispatch(loadContacts());
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ContactsComponent/>
+  </Provider>, document.getElementById('root'));
